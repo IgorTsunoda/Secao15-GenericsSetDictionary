847ed2be7b5af9f9effadbfb33b6962f83a4293a
Igor Tsunoda <igortaikitsunoda@gmail.com>
2024-05-13 21:39:56 -0300

    GetHashCodeAndEquals

---
+++ GenericsLimitationSolved/GenericsLimitationSolved/Program.cs
@@ -0,0 +1,30 @@
+﻿using GenericsLimitationSolved.Entities;
+using GenericsLimitationSolved.Services;
+using System.Globalization;
+
+namespace GenericsLimitationSolved
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            Console.WriteLine("Enter the number of products: ");
+            int N = 0;
+            N = int.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = Console.ReadLine();
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(vect[0], price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product p = calculationService.Max(list);
+            Console.WriteLine("Most Expensive: ");
+            Console.WriteLine(p);
+        }
+    }
+}
---
+++ GetHashCode e Equals/GetHashCode e Equals/Program.cs
@@ -0,0 +1,34 @@
+﻿using GetHashCode_e_Equals.Entities;
+
+namespace GetHashCode_e_Equals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Maria";
+            string b = "Igor";
+
+            Console.WriteLine(a.Equals(b));
+
+            string g = "Fernanda";
+            string h = "Torresminho";
+
+            Console.WriteLine(g.GetHashCode());
+            Console.WriteLine(h.GetHashCode());
+
+            //GetHashCode e Equals personalizados.
+
+            Client c1 = new Client {Name="Maria", Email="Maria@gmail.com" };
+            Client c2 = new Client { Name = "Fernanda", Email = "Fernanda@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+            //Console.WriteLine(a == b); isso, não é a mesma coisa que um equals, aqui,
+            //ele esta comparando se o endereço do ponteiro é o mesmo. portanto o resultado deveria ser falso
+
+        }
+    }
+}

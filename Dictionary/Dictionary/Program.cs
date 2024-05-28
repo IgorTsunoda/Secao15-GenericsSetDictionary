f31d3647e9fcbea07b35e548a07edaad99ae9ddb
Igor Tsunoda <igortaikitsunoda@gmail.com>
2024-05-27 21:29:50 -0300

    adding Dictionary example

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "9749864651";
+            cookies["phone"] = "6542682612";
+
+            Console.WriteLine(cookies["email"]);
+            cookies.Remove("email");
+
+            Console.WriteLine("Phone number "+ cookies["phone"]);
+
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine("Email: " + cookies["email"]);
+            }
+            else { Console.WriteLine("There is no 'email' key"); }
+            Console.WriteLine("Size: " + cookies.Count);
+
+            Console.WriteLine("ALL COOKIES:");
+            foreach(KeyValuePair<string, string> item in cookies) {
+                Console.WriteLine(item.Key + " : " + item.Value);
+            }
+        }
+    }
+}
---
+++ Testes/Testes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace Testes
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            // Suponha que esta é a data decimal recebida no formato yyyyMMdd
+            decimal dateDecimal = 20240517m;
+
+            // Converter o decimal para string
+            string dateString = dateDecimal.ToString();
+
+            // Analisar a string para criar um objeto DateTime
+            DateTime parsedDate = DateTime.ParseExact(dateString, "yyyyMMdd", CultureInfo.InvariantCulture);
+
+            // Formatar o objeto DateTime no formato "dd.MM.yy"
+            string formattedDate = parsedDate.ToString("dd.MM.yy");
+
+            // Exibir a data formatada
+            Console.WriteLine("Data formatada: " + formattedDate); // Ex: 17.05.24
+        }
+    }
+}
+
+
